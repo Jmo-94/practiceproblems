779627cdd0fc2d9064bc485c568facbabdcd9b1c
Jorge <Jorgeemolina10@gmail.com>
2023-02-25 14:13:45 -0500

    Problems im working on

---
+++ farenheit.py
@@ -0,0 +1,111 @@
+# def convertToFahrenheit(degreesCelsius):
+#     Fahrenheit = degreesCelsius * (9/5) + 32
+#     return int(Fahrenheit)
+    
+#print(type(convertToFahrenheit(0)))
+#print(convertToFahrenheit(0))
+
+
+
+# def convertToCelsius(degreesFahrenheit):
+#     Celsius = (degreesFahrenheit - 32) * (5/9)
+#     return Celsius
+
+#print(convertToCelsius(5))
+          
+#convertToCelsius(0)
+
+# assert convertToCelsius(0) == -17.77777777777778
+
+# assert convertToCelsius(180) == 82.22222222222223
+
+# assert convertToFahrenheit(0) == 32
+
+# assert convertToFahrenheit(100) == 212
+
+# assert convertToCelsius(convertToFahrenheit(15)) == 15
+
+
+# def isOdd(one):
+#     if one % 2 == 1:
+#         return True
+#     else:
+#         return False
+    
+
+# def isEven(integer):
+#     if integer % 2 == 0:
+#         return True
+#     else:
+#         return False
+    
+# assert isOdd(42) == False
+
+# assert isOdd(9999) == True
+
+# assert isOdd(-10) == False
+
+# assert isOdd(-11) == True
+
+# assert isOdd(3.1415) == False
+
+# assert isEven(42) == True
+
+# assert isEven(9999) == False
+
+# assert isEven(-10) == True
+
+# assert isEven(-11) == False
+
+# assert isEven(3.1415) == False   
+       
+    
+# def area(length, width):
+#     area = length * width
+#     return area
+
+# print (area(5,6))
+
+# def perimeter(length , width):
+#     peri = 2*(length) + 2*(width)
+#     return peri
+
+# print (perimeter(5,6))
+
+# def volume(l , w , h):
+#     vol = l * w * h
+#     return vol
+
+# print (volume(5,6,5))
+
+# def surfaceArea(l,w,h):
+#     SA = (l * w *2)+(l * h * 2)+(w * h *2)
+#     return SA
+
+# print (surfaceArea(4,5,6))
+
+# assert area(10, 10) == 100
+
+# assert area(0, 9999) == 0
+
+# assert area(5, 8) == 40
+
+# assert perimeter(10, 10) == 40
+
+# assert perimeter(0, 9999) == 19998
+
+# assert perimeter(5, 8) == 26
+
+# assert volume(10, 10, 10) == 1000
+
+# assert volume(9999, 0, 9999) == 0
+
+# assert volume(5, 8, 10) == 400
+
+# assert surfaceArea(10, 10, 10) == 600
+
+# assert surfaceArea(9999, 0, 9999) == 199960002
+
+# assert surfaceArea(5, 8, 10) == 340
+
+
